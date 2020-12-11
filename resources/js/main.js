2835e7237e193d0a14ef41e755bdadd5edae27e7
Nicollas de Oliveira <xnicobr@hotmail.com>
2020-12-11 11:32:40 -0300

    Finished template

--- resources/js/main.js
+++ resources/js/main.js
@@ -20,4 +20,37 @@ $(document).ready(function () {
       return false;
     });
   });
+
+  // course program functions
+  $('#courseProgramButton').click(function () {
+    courseProgram = $('#courseProgram').val();
+    $('#courseProgramListGroup').append("<li>" + courseProgram + "<a href='javascript:void(0)' class='clear' style='color:red;'><i>❌</i> </a></li>");
+
+    $("#courseProgramListGroup a.clear").click(function () {
+      $(this).parent().remove();
+      return false;
+    });
+  });
+
+  // tools functions
+  $('#toolsButton').click(function () {
+    tools = $('#tools').val();
+    $('#toolsListGroup').append("<li>" + tools + "<a href='javascript:void(0)' class='clear' style='color:red;'><i>❌</i> </a></li>");
+
+    $("#toolsListGroup a.clear").click(function () {
+      $(this).parent().remove();
+      return false;
+    });
+  });
+
+  // evaluation process functions
+  $('#evaluationProcessButton').click(function () {
+    evaluationProcess = $('#evaluationProcess').val();
+    $('#evaluationProcessListGroup').append("<li>" + evaluationProcess + "<a href='javascript:void(0)' class='clear' style='color:red;'><i>❌</i> </a></li>");
+
+    $("#evaluationProcessListGroup a.clear").click(function () {
+      $(this).parent().remove();
+      return false;
+    });
+  });
 });
